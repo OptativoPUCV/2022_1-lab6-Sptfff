935b44fb608ef4d5dd1c59735497db6839249de5
MATIASIGNACIO16 <>
2022-05-13 21:46:14 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -53,13 +53,8 @@ int is_valid(Node* n){
 
 
   //PARA EL 3X3
-  int k=4,p; 
-  for(p=0;p<9;p++){
-      int i=3*(k/3) + (p/3) ;
-      int j=3*(k%3) + (p%3) ;
-      printf("%d ",n->sudo[i][j]);
-      if(p%3 == 2) printf("\n");
-  }
+
+  
   return 1;
 }
 
